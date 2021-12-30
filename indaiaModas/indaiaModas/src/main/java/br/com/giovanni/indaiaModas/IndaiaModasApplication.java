7fd04b7d622be8c990df1a701094e5b4587e643f
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-12-29 22:56:41 -0300

    Criando a pasta padrão do projeto

---
+++ indaiaModas/indaiaModas/src/main/java/br/com/giovanni/indaiaModas/IndaiaModasApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovanni.indaiaModas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndaiaModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndaiaModasApplication.class, args);
+	}
+
+}
---
+++ indaiaModas/indaiaModas/src/test/java/br/com/giovanni/indaiaModas/IndaiaModasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanni.indaiaModas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndaiaModasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
